ae272f24ece4525256ca1f5b351bb71b722a4513
Sufyān <132440690+Sofiane5900@users.noreply.github.com>
2024-12-24 14:20:32 +0100

    ajout clase mot & pendu

---
+++ ExoPendu.NET/Mot.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExoPendu.NET
+{
+
+    internal class Mot
+    {
+
+        private List<string> _motATrouverList;
+
+        public List<string> motATrouverList { get { return _motATrouverList; } }
+
+        public Mot (string motATrouverList)
+        {
+            _motATrouverList = new List<string> {"ordinateur" , "programmation" , "csharp" , "dotnet"};
+        }
+
+        void GenererMot()
+        {
+            Random random = new Random();
+            int index = random.Next(0, 4);
+        }
+    }
+}

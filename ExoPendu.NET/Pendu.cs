fab9ea2ff741913d727bddbb359e906824132575
Sufyān <132440690+Sofiane5900@users.noreply.github.com>
2024-12-24 14:01:15 +0100

    Ajoutez des fichiers projet.

---
+++ ExoPendu.NET/Pendu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExoPendu.NET
+{
+    internal class Pendu
+    {
+        private string _masque;
+        private int _nombreEssai;
+
+        public string masque { get => _masque; set => masque = _masque; }
+        public int nombreEssai { get => _nombreEssai; set => nombreEssai = _nombreEssai; }
+
+
+        public Pendu(string masque, int nombreEssai)
+        {
+            _masque = masque;
+            _nombreEssai = nombreEssai;
+        }
+    }
+}
---
+++ ExoPendu.NET/Program.cs
@@ -0,0 +1,2 @@
+﻿using ExoPendu.NET;
+

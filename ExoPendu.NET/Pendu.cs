fbd513d769a0ef849053b559c87d24691f117ca9
Sufyān <132440690+Sofiane5900@users.noreply.github.com>
2024-12-24 20:14:11 +0100

    reformat csharpier

--- ExoPendu.NET/Pendu.cs
+++ ExoPendu.NET/Pendu.cs
@@ -7,10 +7,8 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-
 namespace ExoPendu.NET
 {
-
     internal class Pendu
     {
         private string _masque;
@@ -20,19 +18,34 @@ namespace ExoPendu.NET
         private char[] _masqueArray;
         private bool _partieGagner;
 
-
-        public string Masque { get => _masque; set => Masque = _masque; }
-        public int NombreEssai { get => _nombreEssai; set => _nombreEssai = value; }
-        public string MotCacher { get => _motCacher; set => _motCacher = value; }
-        public bool PartieGagner { get => _partieGagner; set => _partieGagner = value; }
+        public string Masque
+        {
+            get => _masque;
+            set => Masque = _masque;
+        }
+        public int NombreEssai
+        {
+            get => _nombreEssai;
+            set => _nombreEssai = value;
+        }
+        public string MotCacher
+        {
+            get => _motCacher;
+            set => _motCacher = value;
+        }
+        public bool PartieGagner
+        {
+            get => _partieGagner;
+            set => _partieGagner = value;
+        }
 
         public Pendu()
         {
             // L'utilisateur a 10 essais par default.
             _nombreEssai = 10;
         }
-         
-       public void GenererMasque()
+
+        public void GenererMasque()
         {
             // J'instancie un objet mot de type Mot
             Mot mot = new Mot();
@@ -41,23 +54,20 @@ namespace ExoPendu.NET
             MotCacher = _motATrouver;
             // Je transforme mon _motATrouver en une variable tableau de type char
             _charArray = _motATrouver.ToCharArray();
-            // Je déclare que _masqueArray est un nouveau tableau de caractères de la meme longueur que _charArray.Length 
-            _masqueArray = new string ('*', _charArray.Length).ToCharArray(); // ToCharArray en tant que Méthode pour convertir une chaine en tableau de char[]
+            // Je déclare que _masqueArray est un nouveau tableau de caractères de la meme longueur que _charArray.Length
+            _masqueArray = new string('*', _charArray.Length).ToCharArray(); // ToCharArray en tant que Méthode pour convertir une chaine en tableau de char[]
             // Je converti mon tableau de caractére en chaine de caractéres
-            
         }
 
         public void TestChar()
         {
-            
-            // Tant que mon nombre d'essai est différent (!=) de 0 
+            // Tant que mon nombre d'essai est différent (!=) de 0
             while (NombreEssai != 0)
             {
-            
-            Console.Write("Veuillez saisir une lettre : ");
-            char c;
-            bool sucessChar = char.TryParse(Console.ReadLine(), out c);
-            if(!sucessChar)
+                Console.Write("Veuillez saisir une lettre : ");
+                char c;
+                bool sucessChar = char.TryParse(Console.ReadLine(), out c);
+                if (!sucessChar)
                 {
                     Console.Clear();
                     Console.ForegroundColor = ConsoleColor.Red;
@@ -68,34 +78,37 @@ namespace ExoPendu.NET
                 for (int i = 0; i < _charArray.Length; i++)
                 {
                     // A chaque itération, verifie que _charArray contiens le caractére de la saisie de l'user, et affiche un message.
-                    if( _charArray[i] == c)
+                    if (_charArray[i] == c)
                     {
-                        // Je remplace la saisie de l'utilisateur par la lettre correspondante dans le tableau 
+                        // Je remplace la saisie de l'utilisateur par la lettre correspondante dans le tableau
                         _masqueArray[i] = c;
                         // Je convertis mon tableau en une chaine de caractére pour pouvoir l'afficher.
+                        Console.ForegroundColor = ConsoleColor.Blue;
                         Console.WriteLine($"Le mot à trouver : {new string(_masqueArray)}");
+                        Console.ResetColor();
+
                         if (!_masqueArray.Contains('*'))
                         {
                             TestWin();
                         }
                     }
-                   
                 }
+                NombreEssai--;
+                AffichageEtat();
             }
         }
 
-        
         public void TestWin()
         {
-           
-                PartieGagner = true;
-                Console.WriteLine($"Bravo, vous avez gagner la partie! le mot caché etait {MotCacher}");
+            PartieGagner = true;
+            Console.WriteLine($"Bravo, vous avez gagner la partie! le mot caché etait {MotCacher}");
         }
 
         public void AffichageEtat()
         {
-            
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine($"Il vous reste {NombreEssai} essais.");
+            Console.ResetColor();
         }
     }
 }
